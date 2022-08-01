8eb57fdbf4c5157795088174f21d2c687088adc6
Michael Gmelin <freebsd@grem.de>
2022-08-01 16:56:50 +0200

    Script extension and cleanup
    - Switch to using debian packages whenever possible
    - Allow specifying your own mfsbsd image
    - Make daemonize optional
    - Some script cleanup

--- depenguinme.sh
+++ depenguinme.sh
@@ -10,6 +10,11 @@
 # v0.0.3  2022-07-31  bretton depenguin.me
 #  use uefi.bin as bios to enable support of >2TB disks
 #  use hardened mfsbsd image and copy in imported keys
+#
+# v0.0.4  2022-08-01  grembo depenguin.me
+#  use more official packages
+#  add options
+#  some cleanup
 
 # this script must be run as root
 if [ "$EUID" -ne 0 ]; then
@@ -17,55 +22,88 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-usage() { echo "Usage: $0 [-k /path/to/authorized_keys] [-n http://host.domain/keys.pub]" 1>&2; exit 1; }
+set -eo pipefail
 
-while getopts ":k:n:" flags; do
-    case "${flags}" in
-        k)
-            AUTHKEYURL=""
-            AUTHKEYFILE=$(realpath "${OPTARG}")
-            ;;
-        n)
-            AUTHKEYURL="${OPTARG}"
-            AUTHKEYFILE=""
-            ;;
-        *)
-            usage
-            ;;
-    esac
+exit_error() {
+	echo "$*" 1>&2
+	exit 1;
+}
+
+usage() {
+	cat <<-EOF
+	Usage: $(basename "${BASH_SOURCE[0]}") [-hd] [-k authorized_keys] [-n URL] [-m url]
+
+	  -h Show help
+	  -d daemonize
+	  -k /path/to/authorized_keys : Path to authorized_keys file
+	  -n : https://host.domain/authorized_keys : URL to authorized key file
+	  -m : URL of mfsbsd image (defaults to image on https://dependuin.me)
+
+	  At least one of -k and -n is mandatory.
+	EOF
+}
+
+AUTHKEYURL=
+AUTHKEYFILE=
+DAEMONIZE=NO
+MFSBSDISO="https://depenguin.me/files/mfsbsd-13.1-RELEASE-amd64.iso"
+
+while getopts "hdk:m:n:" flags; do
+	case "${flags}" in
+	h)
+		usage
+		exit 0
+		;;
+	d)
+		DAEMONIZE=YES
+		;;
+	k)
+		AUTHKEYURL=""
+		AUTHKEYFILE=$(realpath "${OPTARG}")
+		;;
+
+	m)
+		MFSBSDISO="${OPTARG}"
+		;;
+	n)
+		AUTHKEYURL=${OPTARG}
+		AUTHKEYFILE=""
+		;;
+	*)
+		exit_error "$(usage)"
+		;;
+	esac
 done
 shift "$((OPTIND-1))"
 
-#if [ -z "${k}" ] || [ -z "${n}" ]; then
-#    usage
-#fi
+if [ -z "${AUTHKEYFILE}" ] && [ -z "${AUTHKEYURL}" ]; then
+	exit_error "$(usage)"
+fi
 
 # install required packages
 apt-get update
 apt-get install -y mkisofs
-###
-# disabled for now as not sure about bios stuff, using statically-compiled qemu with copied-in uefi bios instead
-# apt-get install -y qemu
-###
+apt-get install -y qemu
 
 # vars, do not adjust unless you know what you're doing for this script
-QEMUBASE="/tmp"
+QEMUBASE="/tmp/depenguinme"
 USENVME=0
 MYPRIMARYIP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 MYVNC="127.0.0.1:1"
 MYVGA="std"   # could be qxl but not enabled for the static-qemu binary
-MYBIOS="uefi.bin"  # default is bios-256k.bin, uefi.bin is downloaded separately
-MYKEYMAP="keymaps/en-us"
+MYBIOS="/usr/share/ovmf/OVMF.fd"
+MYKEYMAP="en-us"
 MYLOG="${QEMUBASE}/qemu-depenguin.log"
-MYISOAUTH="myiso.iso"
+MYISOAUTH="${QEMUBASE}/myiso.iso"
+MFSBSDFILE="${QEMUBASE}/$(echo "$MFSBSDISO" | sha256sum | awk '{print $1}').iso"
+
+mkdir -p "$QEMUBASE"
 
 ###
 # Custom build mfsbsd file
 ###
-MFSBSDISO="https://depenguin.me/files/mfsbsd-13.1-RELEASE-amd64.iso"
-export MFSBSDISO
-MFSBSDFILE=$(echo "${MFSBSDISO}"|awk -F "/" '{print $5}')
-export MFSBSDFILE
+export MFSBSDFILE  # XXX: why export?
+export MFSBSDISO  # XXX: why export?
 
 ###
 # 2022-07-28
@@ -83,263 +121,184 @@ export MFSBSDFILE
 # + https://depenguin.me/files/vkvm.tar.gz
 ###
 
-# static qemu files
-QEMUSTATICSRC="https://depenguin.me/files/vkvm.tar.gz"
-export QEMUSTATICSRC
-QEMUSTATICFILE=$(echo "${QEMUSTATICSRC}"|awk -F "/" '{print $5}')
-export QEMUSTATICFILE
-QEMUBIN="${QEMUBASE}/qemu-system-x86_64"
-QEMUALTBIOSSRC="https://depenguin.me/files/uefi.tar.gz"
-export QEMUALTBIOSSRC
-QEMUALTBIOSFILE=$(echo "${QEMUALTBIOSSRC}"|awk -F "/" '{print $5}')
-export QEMUALTBIOSFILE
+QEMUBIN=$(which qemu-system-x86_64)
 
 # change directory to /tmp to continue
-cd "${QEMUBASE}" || exit
+cd "${QEMUBASE}" || exit_error "Could not cd to $QEMUBASE"
 
 # setup or retrieve authorised keys
-if [ -z "${AUTHKEYURL}" ] && [ -z "${AUTHKEYFILE}" ]; then
-    echo "No keys selected, you should get the usage message instead of this error"
-    exit 1
-elif [ -z "${AUTHKEYURL}" ] && [ -n "${AUTHKEYFILE}" ]; then
-    # if no url, yet with file link, copy file to holding file
-    cp -f "${AUTHKEYFILE}" COPYKEY.pub
-elif [ -n "${AUTHKEYURL}" ] && [ -z "${AUTHKEYFILE}" ]; then
-    # if url, but no file link, retrieve from url, to holding file
-    wget -qc "${AUTHKEYURL}" -O COPYKEY.pub
-elif [ -n "${AUTHKEYURL}" ] && [ -n "${AUTHKEYFILE}" ]; then
-    # if url and file link, combine both and upload
-    wget "${AUTHKEYURL}" -O COPYKEY.pub
-    cat "${AUTHKEYFILE}" >> COPYKEY.pub
+: >COPYKEY.pub
+
+if [ -n "${AUTHKEYFILE}" ]; then
+	cat "${AUTHKEYFILE}" >>COPYKEY.pub
 fi
+
+if [ -n "${AUTHKEYURL}" ]; then
+	wget -qO - "${AUTHKEYURL}" >>COPYKEY.pub
+fi
+
+[ -s COPYKEY.pub ] || exit_error "Authorized key sources are empty"
 
 # temp solution to make iso with authorized_keys
 mkdir -p "${QEMUBASE}"/myiso
 if [ -f "${QEMUBASE}"/COPYKEY.pub ] && [ -d "${QEMUBASE}"/myiso ]; then
-    cp -f COPYKEY.pub "${QEMUBASE}"/myiso/mfsbsd_authorized_keys
+	cp -f COPYKEY.pub "${QEMUBASE}"/myiso/mfsbsd_authorized_keys
 else
-    echo "Error copying COPYKEY.pub to myiso/mfsbsd_authorized_keys"
-    exit 1
+	exit_error "Error copying COPYKEY.pub to myiso/mfsbsd_authorized_keys"
 fi
 
 # create iso image with the public keys
 if [ -f "${QEMUBASE}"/myiso/mfsbsd_authorized_keys ]; then
-    /usr/bin/genisoimage -v -J -r -V MFSBSD_AUTHKEYS \
-      -o "${QEMUBASE}"/"${MYISOAUTH}" \
-      "${QEMUBASE}"/myiso/mfsbsd_authorized_keys
+	/usr/bin/genisoimage -v -J -r -V MFSBSD_AUTHKEYS \
+	  -o "${MYISOAUTH}" "${QEMUBASE}"/myiso/mfsbsd_authorized_keys
 else
-    echo "Missing myiso/mfsbsd_authorized_keys"
-    exit 1
+	exit_error "Missing myiso/mfsbsd_authorized_keys"
 fi
 
-# download mfsbsd
-wget -qc "${MFSBSDISO}"
+# download mfsbsd image
+wget -qc -O "${MFSBSDFILE}" "${MFSBSDISO}" || exit_error "Could not download mfsbsd image"
 
-# download qemu static
-wget -qc "${QEMUSTATICSRC}"
-
-if [ -f "${QEMUSTATICFILE}" ]; then
-    tar -xzvf "${QEMUSTATICFILE}"
-else
-    echo "missing ${QEMUSTATICFILE}"
-    exit
-fi
-
-# check if qemu-static file exists
+# check if qemu binay exists
 echo "Checking if ${QEMUBIN} exists"
-stat "${QEMUBIN}" || exit
-
-# download UEFI bios
-wget -qc "${QEMUALTBIOSSRC}"
-
-# extract the alternate uefi bios
-if [ -f "${QEMUALTBIOSFILE}" ]; then
-    cd share/qemu || exit
-    tar -xzf "${QEMUBASE}"/"${QEMUALTBIOSFILE}"
-    cd "${QEMUBASE}" || exit
-else
-    echo "Missing uefi.tar.gz"
-    exit 1
-fi
+stat "${QEMUBIN}" || exit_error "Couldn't find qemu binary $QEMUBIN"
 
 # check if sda & sdb
+echo "Searching sd[ab]"
+set +e
 checkdiskone=$(lsblk |grep sda |head -1)
 notcdrom=$(lsblk |grep sdb |grep cdrom)
 if [ -z "${notcdrom}" ]; then
-    checkdisktwo=$(lsblk |grep sdb |head -1)
+	checkdisktwo=$(lsblk |grep sdb |head -1)
 else
-    checkdisktwo=""
+	checkdisktwo=""
 fi
+set -e
 
 # check for nvme, hetzner specific
+set +e
+echo "Searching nvme"
 mycheck=$(which nvme)
 if [ -n "${mycheck}" ]; then
-    existsnvme=$(nvme list |grep -c "/dev/nvme")
-    if [ "$existsnvme" -ge 2 ]; then
-        checknvmeone=$(lsblk |grep nvme0n1 |head -1)
-        checknvmetwo=$(lsblk |grep nvme1n1 |head -1)
-        USENVME=1
-    elif [ "$existsnvme" -eq 1 ]; then
-        checknvmeone=$(lsblk |grep nvme0n1 |head -1)
-        USENVME=1
-    elif [ -z "$existsnvme" ]; then
-        USENVME=0
-    fi
+	existsnvme=$(nvme list | grep -c "/dev/nvme")
+	if [ "$existsnvme" -ge 2 ]; then
+		checknvmeone=$(lsblk |grep nvme0n1 |head -1)
+		checknvmetwo=$(lsblk |grep nvme1n1 |head -1)
+		USENVME=1
+	elif [ "$existsnvme" -eq 1 ]; then
+		checknvmeone=$(lsblk |grep nvme0n1 |head -1)
+		USENVME=1
+	elif [ -z "$existsnvme" ]; then
+		USENVME=0
+	fi
 fi
+set -e
+
+# basic arguments to qemu
+qemu_args=(\
+  -net nic \
+  -net "user,hostfwd=tcp::1022-:22" \
+  -m 1024M \
+  -rtc base=localtime \
+  -enable-kvm \
+  -cpu host \
+  -M pc \
+  -smp 1 \
+  -bios "${MYBIOS}" \
+  -vga "${MYVGA}" \
+  -k "${MYKEYMAP}" \
+  -cdrom "${MFSBSDFILE}" \
+  -drive file="${MYISOAUTH},media=cdrom" \
+  -boot once=d \
+  -vnc "${MYVNC}" \
+  -D "${MYLOG}"\
+)
+
+if [ "$DAEMONIZE" = "YES" ]; then
+	qemu_args+=(-daemonize)
+fi
+
+disks=()
 
 # start qemu-static with parameters
 if [ "$USENVME" -eq 0 ]; then
-    if [ -n "$checkdiskone" ] && [ -n "$checkdisktwo" ]; then
-    echo ""
-    echo "NOTICE: using sda and sdb"
-    echo ""
-
-    cd "${QEMUBASE}"/share/qemu || exit
-
-        "${QEMUBIN}" \
-          -net nic \
-          -net user,hostfwd=tcp::1022-:22 \
-          -m 1024M \
-          -localtime \
-          -enable-kvm \
-          -cpu host \
-          -M pc \
-          -smp 1 \
-          -bios "${MYBIOS}" \
-          -vga "${MYVGA}" \
-          -usbdevice tablet \
-          -k "${MYKEYMAP}" \
-          -cdrom "${QEMUBASE}/${MFSBSDFILE}" \
-          -hda /dev/sda \
-          -hdb /dev/sdb \
-          -drive file="${QEMUBASE}/${MYISOAUTH}",media=cdrom \
-          -boot once=d \
-          -vnc "${MYVNC}" \
-          -D "${MYLOG}"\
-          -daemonize
-    elif [ -n "$checkdiskone" ] && [ -z "$checkdisktwo" ]; then
-    echo ""
-    echo "NOTICE: using sda only"
-    echo ""
-
-    cd "${QEMUBASE}"/share/qemu || exit
-
-        "${QEMUBIN}" \
-          -net nic \
-          -net user,hostfwd=tcp::1022-:22 \
-          -m 1024M \
-          -localtime \
-          -enable-kvm \
-          -cpu host \
-          -M pc \
-          -smp 1 \
-          -bios "${MYBIOS}" \
-          -vga "${MYVGA}" \
-          -usbdevice tablet \
-          -k "${MYKEYMAP}" \
-          -cdrom "${QEMUBASE}/${MFSBSDFILE}" \
-          -hda /dev/sda \
-          -drive file="${QEMUBASE}/${MYISOAUTH}",media=cdrom \
-          -boot once=d \
-          -vnc "${MYVNC}" \
-          -D "${MYLOG}"\
-          -daemonize
-   fi
+	if [ -n "$checkdiskone" ] && [ -n "$checkdisktwo" ]; then
+		printf "\nNOTICE: using sda and sdb\n\n"
+		disks=(
+		  -hda /dev/sda \
+		  -hdb /dev/sdb \
+		)
+	elif [ -n "$checkdiskone" ] && [ -z "$checkdisktwo" ]; then
+		printf "\nNOTICE: using sda only\n\n"
+		disks=(
+		  -hda /dev/sda \
+		)
+	fi
 elif [ "$USENVME" -eq 1 ]; then
-    if [ -n "$checknvmeone" ] && [ -n "$checknvmetwo" ]; then
-    echo ""
-    echo "NOTICE: using nvme1 and nvme2"
-    echo ""
-
-    cd "${QEMUBASE}"/share/qemu || exit
-
-        "${QEMUBIN}" \
-          -net nic \
-          -net user,hostfwd=tcp::1022-:22 \
-          -m 1024M \
-          -localtime \
-          -enable-kvm \
-          -cpu host \
-          -M pc \
-          -smp 1 \
-          -bios "${MYBIOS}" \
-          -vga "${MYVGA}" \
-          -usbdevice tablet \
-          -k "${MYKEYMAP}" \
-          -cdrom "${QEMUBASE}/${MFSBSDFILE}" \
-          -hda /dev/nvme0n1 \
-          -hdb /dev/nvme1n1 \
-          -drive file="${QEMUBASE}/${MYISOAUTH}",media=cdrom \
-          -boot once=d \
-          -vnc "${MYVNC}" \
-          -D "${MYLOG}" \
-          -daemonize
-    elif [ -n "$checknvmeone" ] && [ -z "$checknvmetwo" ]; then
-    echo ""
-    echo "NOTICE: using nvme1 only"
-    echo ""
-
-    cd "${QEMUBASE}"/share/qemu || exit
-
-        "${QEMUBIN}" \
-          -net nic \
-          -net user,hostfwd=tcp::1022-:22 \
-          -m 1024M \
-          -localtime \
-          -enable-kvm \
-          -cpu host \
-          -M pc \
-          -smp 1 \
-          -bios "${MYBIOS}" \
-          -vga "${MYVGA}" \
-          -usbdevice tablet \
-          -k "${MYKEYMAP}" \
-          -cdrom "${QEMUBASE}/${MFSBSDFILE}" \
-          -hda /dev/nvme0n1 \
-          -drive file="${QEMUBASE}/${MYISOAUTH}",media=cdrom \
-          -boot once=d \
-          -vnc "${MYVNC}" \
-          -D "${MYLOG}" \
-          -daemonize
-   fi
+	if [ -n "$checknvmeone" ] && [ -n "$checknvmetwo" ]; then
+		printf "\nNOTICE: using nvme0 and nvme1\n\n"
+		disks=(
+		  -hda /dev/nvme0n1 \
+		  -hdb /dev/nvme1n1 \
+		)
+	elif [ -n "$checknvmeone" ] && [ -z "$checknvmetwo" ]; then
+		printf "\nNOTICE: using nvme0 only\n\n"
+		disks=(
+		  -hda /dev/nvme0n1 \
+		)
+	fi
 fi
 
-# let the system boot, yes we need this much time
-# at least 2 minutes with rc.local adjustments
-echo "Please wait, booting..."
-sleep 10
-echo "Please wait, booting..."
-sleep 10
-echo "Please wait, booting..."
-sleep 10
-echo "Please wait, booting..."
-sleep 10
-echo "Please wait, booting..."
-sleep 10
-echo "Please wait, booting..."
-sleep 10
-echo "Please wait, booting..."
-sleep 10
-echo "Please wait, booting..."
-sleep 10
-echo "Please wait, booting..."
-sleep 10
-echo "Please wait, booting..."
-sleep 10
-echo "Please wait, booting..."
-sleep 10
-echo "Almost ready..."
+if [ ${#disks[@]} -eq 0 ]; then
+	exit_error "Could not find any disks"
+fi
 
-# scan for keys
-ssh-keyscan -p 1022 -T 30 127.0.0.1
 
-# we should be able to ssh without a password now
-echo "The system should ready to access with automatic login from a host with the associated private key!"
-echo ""
-echo "  ssh -p 1022 root@${MYPRIMARYIP}"
-echo ""
-echo "If you have difficulty connecting dueh ssh key exchange error then WAIT 2 MINUTES and try again. SSH needs to come up correctly first."
-echo ""
-echo "Run 'zfsinstall -h' for install options, or provision with ansible scripts that cover installation."
-echo ""
-echo "--- DEPENGUINME SCRIPT COMPLETE ---"
+(
+	set +e
+	sleep 2
+
+	# let the system boot, yes we need this much time
+	# at least 2 minutes with rc.local adjustments
+	for c in {0..5}; do
+		echo "Please wait, booting... $((30 - 5*c))s"
+		sleep 5
+	done
+	echo "Waiting for sshd to become available..."
+
+	# scan for keys
+	while ! ssh-keyscan -p 1022 -T 5 127.0.0.1 2>/dev/null; do
+		echo "Waiting for sshd to become available..."
+		sleep 5;
+	done
+
+	# we should be able to ssh without a password now
+	cat <<-EOF
+	The system should ready to access with automatic login from a host with the associated private key!
+
+	  ssh -p 1022 mfsbsd@${MYPRIMARYIP}
+
+	If you have difficulty connecting dueh ssh key exchange error then WAIT 2 MINUTES and try again.
+	SSH needs to come up correctly first.
+
+	Run 'zfsinstall -h' for install options, or provision with ansible scripts that cover installation.
+
+	EOF
+
+	if [ "$DAEMONIZE" = "YES" ]; then
+		echo "--- DEPENGUINME SCRIPT COMPLETE ---"
+	else
+		echo "Press CTRL-C to exit qemu"
+	fi
+)&
+
+keyscan_pid=$!
+function finish {
+	set +e
+	kill $keyscan_pid >/dev/null 2>&1
+}
+trap finish EXIT
+
+echo "Starting qemu..."
+${QEMUBIN} "${qemu_args[@]}"
+
+wait $keyscan_pid
